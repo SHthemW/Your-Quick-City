788fbe72d700255244a48ee1df562050a58e4cd3
SHthemW <2536076378@qq.com>
2023-12-07 18:29:00 +0800

    dev?: API folder

---
+++ Assets/Scripts/API/Map.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yours.QuickCity.Internal;
+
+namespace Yours.QuickCity
+{
+    public static class Map
+    {
+        public static void Generate()
+        {
+
+        }
+    }
+}
