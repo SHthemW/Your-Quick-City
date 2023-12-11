a4b193830d1d7831922fb1a65745c67b8c6e1965
SHthemW <2536076378@qq.com>
2023-12-11 12:48:05 +0800

    dev: optimized project directory II

--- Assets/Scripts/Internals/Interfaces/Map/services/IStuffDetectorDataHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Yours.QuickCity.Internal
-{
-    internal interface IStuffDetectorDataHandler
-    {
-        List<MapTerrainDetector> Detectors { get; }
-    }
-}
