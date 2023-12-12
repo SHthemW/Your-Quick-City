131d33dd44d59ce54482481a0a3bb0eb3d9b9586
SHthemW <2536076378@qq.com>
2023-12-12 13:27:45 +0800

    dev: 封装控制流ForStep和ForeachStep

--- Assets/Scripts/Internals/Interfaces/StepwiseTask.cs
+++ Assets/Scripts/Internals/Interfaces/StepwiseTask.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Yours.QuickCity.Internal
@@ -17,15 +18,60 @@ namespace Yours.QuickCity.Internal
             return (float)_currentStepCount / _targetStepCount * 100;
         }
 
-        protected StepwiseTask(int maxTick) 
-        { 
-            this.maxTick = maxTick; 
-        }
-        protected int _targetStepCount { get; set; } = 1;
-        protected int _currentStepCount { get; set; } = 0;
+        protected int _targetStepCount  = 1;
+        protected int _currentStepCount = 0;
         protected bool IsTimeToReport()
         {
             return tick++ > maxTick || _targetStepCount - _currentStepCount <= tick;
+        }
+
+        protected StepwiseTask(int maxTick) 
+        { 
+            this.maxTick = maxTick; 
+        }       
+        protected IEnumerator ForStep(Action body, int targetStepCount, Action pre = null, Func<bool> endCond = null, Action step = null)
+        {
+            _targetStepCount = targetStepCount;
+
+            for(pre?.Invoke(); endCond == null || endCond.Invoke(); step?.Invoke())
+            {
+                _currentStepCount++;
+
+                try
+                {
+                    body.Invoke();
+                }
+                catch (ContinueException) { continue; }
+                catch (BreakException) { break; }
+
+                if (tick++ > maxTick || _targetStepCount - _currentStepCount <= tick)
+                {
+                    tick = 0;
+                    yield return null;
+                }
+            }
+        }
+        protected IEnumerator ForeachStep<T>(Action<T> body, IEnumerable<T> iter, int stepcnt)
+        {
+            _targetStepCount = stepcnt;
+
+            foreach(T item in iter)
+            {
+                _currentStepCount++;
+
+                try
+                {
+                    body.Invoke(item);
+                }
+                catch (ContinueException) { continue; }
+                catch (BreakException)    { break; }
+
+                if (tick++ > maxTick || _targetStepCount - _currentStepCount <= tick)
+                {
+                    tick = 0;
+                    yield return null;
+                }
+            }
         }
     }
 
