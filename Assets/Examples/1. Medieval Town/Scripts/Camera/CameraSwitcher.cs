c23f059bc56ca6cd616b6def3f37c05c5cd600e2
SHW-MATEBOOK-E\S.H.W <2536076378@qq.com>
2024-02-26 18:21:23 +0800

    dev: camera switcher

---
+++ Assets/Examples/1. Medieval Town/Scripts/Camera/CameraSwitcher.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Yours.QuickCity.Internal
+{
+    public sealed class CameraSwitcher : MonoBehaviour
+    {
+        [SerializeField]
+        private Camera _staticCam;
+
+        [SerializeField]
+        private Camera _dynamicCam;
+
+        public void SwitchToDynamicCamera(bool val)
+        {
+            _staticCam.gameObject.SetActive(!val);
+            _dynamicCam.gameObject.SetActive(val);
+        }
+    }
+}
