dc6a0f6cdd8bbab4fe134bf9e53f09a7de1d9793
SHW-MATEBOOK-E\S.H.W <2536076378@qq.com>
2024-03-04 11:56:25 +0800

    opt: reconstruct abstruction of IShape API.

--- Assets/Scripts/API/Entities/Shape/RectangleShape_SO.cs
+++ Assets/Scripts/API/Entities/Shape/RectangleShape_SO.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
-using Yours.QuickCity.Internal;
 
 namespace Yours.QuickCity.Shape
 {
     [CreateAssetMenu(fileName = "New RectangleShape", menuName = "Map/Shape/Rectangle")]
     public sealed class RectangleShape_SO : Shape_SO
     {
+        // arguments
+
         [Header("Rectangle")]
 
         [SerializeField]
@@ -27,28 +27,13 @@ namespace Yours.QuickCity.Shape
 
         [SerializeField, Range(-0.9f, 1)]
         private float _attachmentDegree = 0;
-
-        protected override sealed string StatsDebugMsg
+     
+        private bool[,] NewSolidRectangleMatrix()
         {
-            get
-            {
-                Histogram<string> extendStat = new("extend count"); 
-
-                extendStat.Construct(
-                    dataSet: _extendLengthCount.ToList(),
-                    getKeyIn: kvp => kvp.Key,
-                    constrToVal: dic => new string('|', dic.Sum(kvp => kvp.Value)),
-                    intervalSize: 1
-                    );
-                return base.StatsDebugMsg + extendStat;
-            }
-        }
-        protected override sealed void GenerateOnMatrix()
-        {
-            int matrix_size_x = _size_x * _emptySizeMultiplier;
-            int matrix_size_y = _size_y * _emptySizeMultiplier;
-
-            _matrix = new bool[matrix_size_x, matrix_size_y];
+            var matrix = new bool[
+                _size_x * _emptySizeMultiplier, 
+                _size_y * _emptySizeMultiplier
+                ];
 
             int startX = (int)((float)(_emptySizeMultiplier - 1) / 2 * _size_x);
             int startY = (int)((float)(_emptySizeMultiplier - 1) / 2 * _size_y);
@@ -59,105 +44,97 @@ namespace Yours.QuickCity.Shape
             // init solid
             for (int x = startX; x < endX; x++)
                 for (int y = startY; y < endY; y++)
-                    _matrix[x, y] = true;
+                    matrix[x, y] = true;
 
-            foreach (var edge in this.Edges)
-                SetConcaveAndConvexity(edge.Key, edge.Value);
+            return matrix;
         }
-
-        private void SetConcaveAndConvexity((int x, int y) coord, EdgeOutsideDir extendDir)
+        private bool[,] GenerateRough(in bool[,] matrix)
         {
-            var roughSeed = RandomRoughFunc(
-                min:            0, 
-                max:            _maxExtendPercent, 
-                minValTendency: _attachmentDegree + 1);
+            Dictionary<int, int> extendLengthCount = new();
 
-            int extendGridNumX = (int)(Mathf.Abs(roughSeed) * _size_x);
-            int extendGridNumY = (int)(Mathf.Abs(roughSeed) * _size_y);
-
-            int x, y;
-
-            const int up = -1;
-            const int left = -1;
-
-            int countKey = (extendDir == EdgeOutsideDir.Up || extendDir == EdgeOutsideDir.Down)
-                ? extendGridNumY
-                : extendGridNumX;
-
-            if (!_extendLengthCount.TryAdd(countKey, 1))
-                _extendLengthCount[countKey]++;
-
-            switch (extendDir)
+            foreach (var edge in (this as IShape).Edges(matrix))
             {
-                case EdgeOutsideDir.Up:
-                    y = coord.y;
-                    for (int count = 0; count < extendGridNumY; count++)
-                    {
-                        _matrix[coord.x, y] = true;
-                        y += up;
-                    }
-                    break;
-                case EdgeOutsideDir.Down:
-                    y = coord.y;
-                    for (int count = 0; count < extendGridNumY; count++)
-                    {
-                        _matrix[coord.x, y] = true;
-                        y -= up;
-                    }
-                    break;
-                case EdgeOutsideDir.Left:
-                    x = coord.x;
-                    for (int count = 0; count < extendGridNumX; count++)
-                    {
-                        _matrix[x, coord.y] = true;
-                        x += left;
-                    }
-                    break;
-                case EdgeOutsideDir.Right:
-                    x = coord.x;
-                    for (int count = 0; count < extendGridNumX; count++)
-                    {
-                        _matrix[x, coord.y] = true;
-                        x -= left;
-                    }
-                    break;
-                default:
-                    throw new ArgumentException();
+                var coord = edge.Key;
+                var extendDir = edge.Value;
+
+                var roughSeed = RandomRough(
+                    min: 0,
+                    max: _maxExtendPercent,
+                    minValTendency: _attachmentDegree + 1);
+
+                int extendGridNumX = (int)(Mathf.Abs(roughSeed) * _size_x);
+                int extendGridNumY = (int)(Mathf.Abs(roughSeed) * _size_y);
+
+                int x, y;
+
+                const int up = -1;
+                const int left = -1;
+
+                int countKey = (extendDir == EdgeOutsideDir.Up || extendDir == EdgeOutsideDir.Down)
+                    ? extendGridNumY
+                    : extendGridNumX;
+
+                if (!extendLengthCount.TryAdd(countKey, 1))
+                    extendLengthCount[countKey]++;
+
+                switch (extendDir)
+                {
+                    case EdgeOutsideDir.Up:
+                        y = coord.y;
+                        for (int count = 0; count < extendGridNumY; count++)
+                        {
+                            matrix[coord.x, y] = true;
+                            y += up;
+                        }
+                        break;
+                    case EdgeOutsideDir.Down:
+                        y = coord.y;
+                        for (int count = 0; count < extendGridNumY; count++)
+                        {
+                            matrix[coord.x, y] = true;
+                            y -= up;
+                        }
+                        break;
+                    case EdgeOutsideDir.Left:
+                        x = coord.x;
+                        for (int count = 0; count < extendGridNumX; count++)
+                        {
+                            matrix[x, coord.y] = true;
+                            x += left;
+                        }
+                        break;
+                    case EdgeOutsideDir.Right:
+                        x = coord.x;
+                        for (int count = 0; count < extendGridNumX; count++)
+                        {
+                            matrix[x, coord.y] = true;
+                            x -= left;
+                        }
+                        break;
+                    default:
+                        throw new ArgumentException();
+                }
+            }
+            return matrix;
+
+            static float RandomRough(float min, float max, float minValTendency)
+            {
+                float randomFloat = (float)new System.Random().NextDouble();
+                float adjustedRandomFloat = (float)Math.Pow(randomFloat, minValTendency);
+
+                return min + adjustedRandomFloat * (max - min);
             }
         }
-        private static float RandomRoughFunc(float min, float max, float minValTendency)
+
+        public override sealed bool[,] GenerateShapeMatrix(float sizeMultiple)
         {
-            float randomFloat         = (float)new System.Random().NextDouble();
-            float adjustedRandomFloat = (float)Math.Pow(randomFloat, minValTendency);
+            var matrix = NewSolidRectangleMatrix();
 
-            return min + adjustedRandomFloat * (max - min);
-        }
+            matrix = this.GenerateRough(matrix);
+            matrix = (this as IShape).ScaleMatrix(matrix, sizeMultiple);
+            matrix = (this as IShape).CropMatrix(matrix);
 
-        // test
-
-        private readonly Dictionary<int, int> _extendLengthCount = new();
-
-        [ContextMenu("Test Random Func")]
-        private void PrintFunctionTestOnConsole()
-        {
-            const int times = 10000;
-
-            List<float> result = new();
-
-            for (int count = 0; count < times; count++)
-            {
-                result.Add(RandomRoughFunc(0, 1, _attachmentDegree + 1));
-            }
-
-            Histogram<string> histogram = new("random");
-
-            histogram.Construct(
-                dataSet: result,
-                getKeyIn: num => num,
-                constrToVal: interval => new string('|', interval.Count() / 20),
-                intervalSize: 0.1f);
-
-            Debug.Log(histogram);
+            return matrix;
         }
     }
 }
--- Assets/Scripts/API/Entities/Shape/Shape_SO.cs
+++ Assets/Scripts/API/Entities/Shape/Shape_SO.cs
@@ -2,203 +2,47 @@
 using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
+using Yours.QuickCity.Internal;
 
 namespace Yours.QuickCity.Shape
 {
     public abstract class Shape_SO : ScriptableObject, IShape
-    {       
-        protected bool[,] _matrix = null;
-        protected abstract void GenerateOnMatrix();
+    {
+        public abstract bool[,] GenerateShapeMatrix(float sizeMultiple);
 
-        bool[,] IShape.GenerateShapeMatrix(float sizeMultiple)
-        {
-            // generate
-            GenerateOnMatrix();
+        #region Test
 
-            // scale
-            ScaleMatrix(sizeMultiple);
+        [Header("<Test Generare Arguments>")]
 
-            // corp
-            CropMatrix();
-
-            return _matrix;
-        }
-
-        protected virtual string ShapeDebugMsg
-        {
-            get
-            {
-                StringBuilder graphMsg = new("graph: \n");
-
-                for (int x = 0; x < _matrix.GetLength(0); x++)
-                {
-                    for (int y = 0; y < _matrix.GetLength(1); y++)
-                    {
-                        graphMsg.Append((_matrix[x, y] ? "■" : "□") + "  ");
-                    }
-                    graphMsg.Append("\n");
-                }
-                return graphMsg.ToString();
-            }
-        }
-        protected virtual string StatsDebugMsg 
-        { 
-            get; 
-        }
-
-        protected Dictionary<(int x, int y), EdgeOutsideDir> Edges
-        {
-            get
-            {
-                int size_x = _matrix.GetLength(0);
-                int size_y = _matrix.GetLength(1);
-
-                var result = new Dictionary<(int x, int y), EdgeOutsideDir>(capacity: size_x * size_y);
-
-                for (int x = 0; x < size_x; x++)
-                {
-                    for (int y = 0; y < size_y; y++)
-                    {
-                        if ((x + 1) >= size_x
-                            || (y + 1) >= size_y
-                            || (x - 1) < 0
-                            || (y - 1) < 0)
-                            continue;
-
-                        var direction = IsEdge(_matrix, (x, y));
-
-                        if (direction != EdgeOutsideDir.NotEdge)
-                            result.Add((x, y), direction);
-                    }
-                }
-                return result;
-            }
-        }
-        protected virtual EdgeOutsideDir IsEdge(bool[,] map, (int x, int y) coord)
-        {
-            (int x, int y) = coord;
-
-            if (!map[x, y])
-                return EdgeOutsideDir.NotEdge;
-
-            bool left = map[x - 1, y];
-            bool right = map[x + 1, y];
-            bool up = map[x, y - 1];
-            bool down = map[x, y + 1];
-
-            if (left && right && up && down)
-                return EdgeOutsideDir.NotEdge;
-
-            if (left && !right)
-                return EdgeOutsideDir.Right;
-
-            else if (!left && right)
-                return EdgeOutsideDir.Left;
-
-            else if (up && !down)
-                return EdgeOutsideDir.Down;
-
-            else if (!up && down)
-                return EdgeOutsideDir.Up;
-
-            return EdgeOutsideDir.NotEdge;
-        }
-
-        protected virtual void ScaleMatrix(float sizeMultiple)
-        {
-            if (sizeMultiple <= 0)
-                throw new ArgumentException();
-
-            if (sizeMultiple == 1)
-                return;
-
-            int originalWidth = _matrix.GetLength(0);
-            int originalHeight = _matrix.GetLength(1);
-
-            int newWidth =  (int)(originalWidth * sizeMultiple);
-            int newHeight = (int)(originalHeight * sizeMultiple);
-
-            bool[,] outputMatrix = new bool[newWidth, newHeight];
-
-            for (int i = 0; i < newWidth; i++)
-            {
-                for (int j = 0; j < newHeight; j++)
-                {
-                    int x = (int)(i / sizeMultiple);
-                    int y = (int)(j / sizeMultiple);
-
-                    // 使用最近邻插值法进行缩放
-                    outputMatrix[i, j] = _matrix[x, y];
-                }
-            }
-            _matrix = outputMatrix;
-        }
-        protected virtual void CropMatrix()
-        {
-            int width  = _matrix.GetLength(0);
-            int height = _matrix.GetLength(1);
-
-            int minX = width;
-            int minY = height;
-            int maxX = 0;
-            int maxY = 0;
-
-            for (int x = 0; x < width; x++)
-            {
-                for (int y = 0; y < height; y++)
-                {
-                    if (_matrix[x, y])
-                    {
-                        minX = Math.Min(minX, x);
-                        minY = Math.Min(minY, y);
-                        maxX = Math.Max(maxX, x);
-                        maxY = Math.Max(maxY, y);
-                    }
-                }
-            }
-
-            int croppedWidth = maxX - minX + 1;
-            int croppedHeight = maxY - minY + 1;
-
-            bool[,] outputMatrix = new bool[croppedWidth, croppedHeight];
-
-            for (int x = 0; x < croppedWidth; x++)
-            {
-                for (int y = 0; y < croppedHeight; y++)
-                {
-                    outputMatrix[x, y] = _matrix[minX + x, minY + y];
-                }
-            }
-            _matrix = outputMatrix;
-        }
-
-        [Header("Test")]
-
-        [SerializeField] float _testScaleMultiple = 1;
+        [SerializeField]
+        private float _argSizeMultiple = 1f;
 
         [ContextMenu("Test Generate")]
         private void GenerateAndPrintOnConsole()
         {
             StringBuilder msg = new();
 
-            GenerateOnMatrix();
-            msg.Append("generate: \n" + ShapeDebugMsg);
+            var matrix = GenerateShapeMatrix(sizeMultiple: _argSizeMultiple);
 
-            ScaleMatrix(_testScaleMultiple);
-            msg.Append("scaled: \n" + ShapeDebugMsg);
-
-            CropMatrix();
-            msg.Append("corped: \n" + ShapeDebugMsg);
-
-            msg.Append("stats: \n" + StatsDebugMsg);
-
+            msg.Append("generate: \n" + GetConsoleGraphByMatrix(matrix));
             Debug.Log(msg.ToString());
         }
+        private string GetConsoleGraphByMatrix(in bool[,] matrix)
+        {
+            StringBuilder graphMsg = new("graph: \n");
 
-    }
+            for (int x = 0; x < matrix.GetLength(0); x++)
+            {
+                for (int y = 0; y < matrix.GetLength(1); y++)
+                {
+                    graphMsg.Append((matrix[x, y] ? "■" : "□") + "  ");
+                }
+                graphMsg.Append("\n");
+            }
 
-    public enum EdgeOutsideDir
-    {
-        NotEdge, Up, Down, Left, Right
+            return graphMsg.ToString();
+        }
+
+        #endregion
     }
 }
